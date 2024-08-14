0e8ec87edca2379f5c30a5772f4ea63bf5ec1669
ramath-x <114470428+ramath-x@users.noreply.github.com>
2024-08-14 16:32:51 +0000

    npm install

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::fallback(function () {
+    return response()->json(['message' => 'Page Not Found.'], 404);
+});
